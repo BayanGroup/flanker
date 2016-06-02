611ab632fcd7e99e210f56ad381f5ec9ded48df9
Mohammad Salehe <salehe+dev@gmail.com>
2016-06-02 14:39:05 +0430

    Bump dev version to dev6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='flanker',
-      version='0.4.39.dev5',
+      version='0.4.39.dev6',
       description='Mailgun Parsing Tools',
       long_description=open('README.rst').read(),
       keywords='',
